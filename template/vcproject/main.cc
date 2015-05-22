16cd6c2b9fc233d6731dd565e938d5c1b2e1d1eb
Meng-Sung Wu <mengsungwu@fortunewhite.org>
2015-05-22 23:21:02 +0800

    Add template for Visual C++

---
+++ template/vcproject/main.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <utility>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+
+typedef long long int ll;
+
+int main(int argc, char *argv[])
+{
+	std::ios_base::sync_with_stdio(false);
+	
+	return 0;
+}
