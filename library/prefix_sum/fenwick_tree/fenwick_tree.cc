6decda5acdb1d33e760a5c0643b0c5a51bf1cd3c
Meng-Sung Wu <mengsungwu@fortunewhite.org>
2015-05-29 21:47:44 +0800

    Prefix Sum - Fenwick Tree

---
+++ library/prefix_sum/fenwick_tree/fenwick_tree.cc
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cassert>
+#include <algorithm>
+#include <numeric>
+#include <utility>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+
+typedef long long int ll;
+
+typedef class FenwickTree {
+public:
+	FenwickTree(int n) {
+		tree.resize(n + 1);
+	}
+
+	FenwickTree(std::vector<ll> data) {
+		tree.resize(data.size() + 1);
+
+		for (int i = 0; i < data.size(); i++) {
+			update(i, data[i]);
+		}
+	}
+
+	void update(int n, ll val) {
+		for (int i = n + 1; i < tree.size(); i += (i & -i)) {
+			tree[i] += val;
+		}
+	}
+
+	ll query(int n) {
+		ll val = 0;
+		for (int i = n + 1; i > 0; i -= (i & -i)) {
+			val += tree[i];
+		}
+		return val;
+	}
+
+	ll query(int n0, int n1) {
+		return query(n1) - query(n0 - 1);
+	}
+
+private:
+	std::vector<ll> tree;
+
+} FenwickTree;
+
+int main(int argc, char *argv[])
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+
+	std::vector<ll> data;
+
+	for (ll i = 0; i < 50; i++) {
+		data.push_back(1LL << i);
+	}
+
+	FenwickTree tree(data);
+
+	/* Test for Fenwick Tree*/
+	for (int i = 0; i < data.size(); i++) {
+		ll prefix_sum = std::accumulate(data.begin(), data.begin() + i + 1, 0LL);
+		std::cout << "prefix_sum[" << i << "] = " << prefix_sum << ", " << tree.query(i) << std::endl;
+		assert(prefix_sum == tree.query(i));
+
+		for (int j = i; j < data.size(); j++) {
+			ll sum = std::accumulate(data.begin() + i, data.begin() + j + 1, 0LL);
+			std::cout << "prefix_sum[" << i << "..." << j << "] = " << sum << ", " << tree.query(i, j) << std::endl;
+			assert(sum == tree.query(i, j));
+		}
+	}
+
+	std::cout << "Pass All Pair Test." << std::endl;
+
+	return 0;
+}
