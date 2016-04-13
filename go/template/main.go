2f820775fe32db2a28e916aa5cda6f83e0e949ee
Meng-Sung Wu <mengsungwu@fortunewhite.org>
2016-04-13 14:40:51 +0800

    Add bufferred output to stdout

--- go/template/main.go
+++ go/template/main.go
@@ -8,11 +8,16 @@ import (
 )
 
 var stdin *bufio.Reader
+var stdout *bufio.Writer
 
 func init() {
-    stdin = bufio.NewReader(os.Stdin)
+	stdin = bufio.NewReader(os.Stdin)
+    stdout = bufio.NewWriter(os.Stdout)
 }
 
 func main() {
 
+
+
+    stdout.Flush()
 }
